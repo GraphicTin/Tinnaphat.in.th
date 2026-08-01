f74e2d00e8af0dc7fa96addf335b50393d3e4165
tins <fgbt4004@gmail.com>
2026-08-02 00:52:06 +0700

    yes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,8 +6,8 @@ import react from '@astrojs/react';
 // https://astro.build/config
 export default defineConfig({
     // 1. Replace with your actual GitHub username and repository name
-    site: 'https://<your-github-username>.github.io',
-    base: '/<your-repo-name>', // e.g., '/my-astro-site'
+    site: 'https://graphictin.github.io',
+    base: 'graphictin', // e.g., '/my-astro-site'
     
     integrations: [react()],
   });
